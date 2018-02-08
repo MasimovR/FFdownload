2f4c0e3b5018193c412be15d09f148c7b089a229
Anar Masimov <>
2018-02-08 21:35:33 +0200

    Returns

---
+++ R/converter.R
@@ -0,0 +1,40 @@
+converter <- function(file) {
+  data <- readLines(file)
+
+  index <- grep(",", data)
+  new_index <- sort(unique(c(index - 1, index)))
+
+  headers <- new_index[!(new_index %in% index)]
+  headers1 <- headers + 1
+  headers2 <- c(headers[-1] - 1,  max(new_index))
+
+  l <- mapply(function(x, y) data[x:y], headers1, headers2, SIMPLIFY = FALSE)
+
+  names <- gsub("(.*) -- .*",  "\\1" , trimws(data[headers]))
+  names <- tolower(gsub(" ", "_", names))
+
+  names(l) <- names
+
+  datatest <- try(lapply(l, function(x) na.omit(read.csv(text = x,  stringsAsFactors = FALSE))), silent = TRUE)
+  if (class(datatest) == "try-error") {
+    datatest <- lapply(l, function(x) na.omit(read.csv(text = x,  stringsAsFactors = FALSE, header = FALSE)))
+  }
+  datatest <- datatest[sapply(datatest, nrow) > 0]
+
+  d <- vector()
+  for (i in 1:length(datatest)) d[i] <- nchar(as.character(datatest[[i]]$X[1])) == 8
+  m <- vector()
+  for (i in 1:length(datatest)) m[i] <- nchar(as.character(datatest[[i]]$X[1])) == 6
+  a <- vector()
+  for (i in 1:length(datatest)) a[i] <- nchar(as.character(datatest[[i]]$X[1])) == 4
+
+  annual  <- lapply(datatest[unlist(a)], function(x) xts(x[,-1], order.by = as.yearmon(as.character(x[, 1]), format = "%Y")) )
+  monthly <- lapply(datatest[unlist(m)], function(x) xts(x[,-1], order.by = as.yearmon(as.character(x[, 1]), format = "%Y%m")))
+  daily <- lapply(datatest[unlist(d)], function(x) xts(x[,-1], order.by = as.Date(as.character(x[, 1]), format = "%Y%m%d")))
+
+
+  return(list(annual = annual, monthly = monthly, daily = daily))
+
+}
+
+
---
+++ R/download_tables.R
@@ -0,0 +1,32 @@
+download_tables <- function(output_file = "data.Rdata") {
+
+  URL <- "http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html"
+  pg <- read_html(URL)
+  Flinks <- html_attr(html_nodes(pg, "a"), "href")
+  Findex <- grep("CSV.zip",Flinks)
+
+  temp <- tempdir()
+  for (i in 1:length(Findex)){
+    Fdest <- gsub("ftp/","",Flinks[Findex[i]])
+    download.file(paste0("http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/",Flinks[Findex[i]]), paste0(temp,"/", Fdest))
+  }
+
+  zip_files <- list.files(temp, full.names = TRUE, pattern = "\\.zip$", ignore.case = TRUE)
+
+  lapply(zip_files, function (x) unzip(zipfile = x, exdir = temp))
+
+  csv_files <- list.files(temp, full.names = TRUE, pattern = "\\.csv$", ignore.case = TRUE)
+
+  vars <- paste0("x_", gsub(paste0(temp, "/(.*)\\..*"), "\\1", csv_files))
+
+  returns <- new.env()
+
+  mapply(function(x, y) assign(x, converter(y), envir = returns), vars,  csv_files)
+
+  returns <- as.list(returns)
+  save(returns, file = output_file)
+}
+
+
+
+
